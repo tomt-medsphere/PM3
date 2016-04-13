5b08a25cedf1f1c82da6840742867339d8180350
Thomas N Tucker Jr <tomt@mbsnetinc.com>
2016-04-12 22:10:48 -0400

    Schema 4

---
+++ PM3.Data/Configuration/AccidentCodeConfiguation.cs
@@ -0,0 +1,22 @@
+﻿using PM3.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM3.Data.Configuration
+{
+    public class AccidentCodeConfiguation : EntityTypeConfiguration<AccidentCode>
+    {
+        public AccidentCodeConfiguation()
+        {
+            ToTable("AccidentCodes");
+            HasKey(p => p.AccidentCodeID);
+            Property(p => p.AccidentCodeID).IsRequired().HasMaxLength(10);
+            Property(p => p.Description).IsRequired().HasMaxLength(60);
+        }
+    }
+}
---
+++ PM3.Data/Configuration/BillingAccountConfiguation.cs
@@ -0,0 +1,26 @@
+﻿using PM3.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM3.Data.Configuration
+{
+    public class BillingAccountConfiguation : EntityTypeConfiguration<BillingAccount>
+    {
+        public BillingAccountConfiguation()
+        {
+            ToTable("BillingAccounts");
+            HasKey(p => p.BillingAccountID);
+            Property(p => p.BillingAccountID).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(p => p.GuarantorID).IsRequired();
+            Property(p => p.BillingAccountTypeID).IsRequired().HasMaxLength(8);
+            Property(p => p.BillingAccountTypeDate).IsRequired();
+            Property(p => p.Description).HasMaxLength(60);
+            Property(p => p.Inactive).IsRequired();
+        }
+    }
+}
