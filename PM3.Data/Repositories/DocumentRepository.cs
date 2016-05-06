264f99dbd0e5fd40a81ab9471a8e0de2fbf91405
Tom Tucker <tomt@mbsnetinc.com>
2016-05-06 17:23:33 -0400

    Something?

---
+++ PM3.Data/Repositories/DocumentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PM3.Data.Infrastructure;
+using PM3.Model.Models;
+
+namespace PM3.Data.Repositories
+{
+    public class DocumentRepository : RepositoryBase<Document>, IDocumentRepository
+    {
+        public DocumentRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+
+    public interface IDocumentRepository : IRepository<Document>
+    {
+    }
+}
---
+++ PM3.Data/Repositories/DocumentTypeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PM3.Data.Infrastructure;
+using PM3.Model.Models;
+
+namespace PM3.Data.Repositories
+{
+    public class DocumentTypeRepository : RepositoryBase<DocumentType>, IDocumentTypeRepository
+    {
+        public DocumentTypeRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }   
+    }
+
+    public interface IDocumentTypeRepository : IRepository<DocumentType>
+    {
+    }
+}
